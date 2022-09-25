e6f3b30753ad94962895bed57f6d0c2e3b3137df
Gastón Caruso <gstn.caruso@gmail.com>
2022-09-25 20:47:06 -0300

    generates method with a string parameter

--- src/__tests__/x.test.ts
+++ src/__tests__/x.test.ts
@@ -1,7 +1,7 @@
 import {BlockStatement, CallExpression, ExpressionStatement, functionDeclaration} from "@babel/types"
 import generate from "@babel/generator";
 
-test("generate method", () => {
+test("generate method with no parameters", () => {
   const expectedCode = `function greet() {\n  console.log("hello!");\n}`
 
   const body: CallExpression = {
@@ -27,6 +27,47 @@ test("generate method", () => {
   const bodyStatement: ExpressionStatement = {type: "ExpressionStatement", expression: body};
   const statement: BlockStatement = {body: [bodyStatement], directives: [], type: "BlockStatement"}
   const newFunction = functionDeclaration({type: "Identifier", name: "greet"}, [], statement);
+
+  const generated = generate(newFunction)
+
+  expect(generated.code).toEqual(expectedCode)
+})
+
+test("generates method with a string parameter", () => {
+  const expectedCode = `function greet(message: string) {\n  console.log(message);\n}`
+
+  const body: CallExpression = {
+    type: "CallExpression",
+    arguments: [{
+      type: "Identifier",
+      name: "message",
+    }],
+    callee: {
+      type: "MemberExpression",
+      object: {
+        type: "Identifier",
+        name: "console"
+      },
+      property: {
+        type: "Identifier",
+        name: "log"
+      },
+      computed: false,
+    },
+  };
+
+  const bodyStatement: ExpressionStatement = {type: "ExpressionStatement", expression: body};
+  const statement: BlockStatement = {body: [bodyStatement], directives: [], type: "BlockStatement"}
+  const newFunction = functionDeclaration({type: "Identifier", name: "greet"}, [{
+    type: "Identifier",
+    name: "message",
+    typeAnnotation: {
+      type: "TSTypeAnnotation",
+      typeAnnotation: {
+        type: "TSStringKeyword"
+      }
+    }
+  }], statement);
 
   const generated = generate(newFunction)
 
