af87e3b4b5b1e05cab64d6debbe6807d75c4955f
Gastón Caruso <gstn.caruso@gmail.com>
2022-09-25 21:49:32 -0300

    generates method with many parameters with string and number types

--- src/__tests__/declareFunction.test.ts
+++ src/__tests__/declareFunction.test.ts
@@ -1,4 +1,4 @@
-import {functionDeclaration, Identifier} from "@babel/types"
+import {functionDeclaration, Identifier, TSTypeAnnotation} from "@babel/types"
 import generate from "@babel/generator";
 
 test("generate method with no parameters", () => {
@@ -11,9 +11,8 @@ test("generate method with no parameters", () => {
 
 test("generates method with a string parameter", () => {
   const expectedCode = `function greet(message: string) {\n  console.log(message);\n}`
-  const parameters = ["message"];
   const name = "greet";
-  const newFunction = declareFunctionWithParameters(name, parameters);
+  const newFunction = declareFunctionWithParameters(name, [{name: "message", type: "string"}]);
 
   const generated = generate(newFunction)
   expect(generated.code).toEqual(expectedCode)
@@ -21,27 +20,46 @@ test("generates method with a string parameter", () => {
 
 test("generates method with many string parameters", () => {
   const expectedCode = `function greet(message: string, exclamation: string) {\n  console.log(message, exclamation);\n}`
-  const newFunction = declareFunctionWithParameters("greet", ["message", "exclamation"]);
+  const newFunction = declareFunctionWithParameters("greet", [
+    {name: "message", type: "string"},
+    {name: "exclamation", type: "string"}
+  ]);
 
   const generated = generate(newFunction)
   expect(generated.code).toEqual(expectedCode)
 })
 
+test("generates method with many parameters with different types", () => {
+  const expectedCode = `function greet(message: string, code: number) {\n  console.log(message, code);\n}`
+  const newFunction = declareFunctionWithParameters("greet", [
+    {name: "message", type: "string"},
+    {name: "code", type: "number"}
+  ]);
+
+  const generated = generate(newFunction)
+  expect(generated.code).toEqual(expectedCode)
+})
+
+type Id = { name: string, type: string };
 const declareFunctionWithNoParameters = (message: string) => declareFunction(message, []);
-const declareFunctionWithParameters = (name: string, parameters: string[]) => declareFunction(name, parameters);
+const declareFunctionWithParameters = (name: string, parameters: Id[]) => declareFunction(name, parameters);
 
-function declareFunction(name: string, parameters: string[]) {
-  const id: (idName: string) => Identifier = (idName: string) => ({type: "Identifier", name: idName});
+function declareFunction(name: string, parameters: Id[]) {
+  const id: (parameter: string) => Identifier = (x) => ({type: "Identifier", name: x});
 
-  const params: Identifier[] = parameters.map((p) => ({
-    ...id(p),
-    typeAnnotation: {
-      type: "TSTypeAnnotation",
+  const params: Identifier[] = parameters.map((p) => {
+    const idType = p.type === "string" ? "TSStringKeyword" : "TSNumberKeyword"
+
+    return ({
+      ...id(p.name),
       typeAnnotation: {
-        type: "TSStringKeyword"
+        type: "TSTypeAnnotation",
+        typeAnnotation: {
+          type: idType as unknown as TSTypeAnnotation,
+        },
       }
-    }
-  }))
+    } as unknown as Identifier);
+  })
 
 
   return functionDeclaration(id(name), params, {
